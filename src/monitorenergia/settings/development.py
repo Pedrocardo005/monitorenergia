cc620e9e284914a3d986cc2fbf3684271ea9eea5
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-15 16:14:31 -0300

    Corrige variaveis de ambiente

--- src/monitorenergia/settings/development.py
+++ src/monitorenergia/settings/development.py
@@ -4,8 +4,8 @@ from monitorenergia.settings.base import *
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DB_HOST = os.environ.get('POSTGRES_PASSWORD')
-DB_PORT = os.environ.get('POSTGRES_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
 POSTGRES_NAME = os.environ.get('POSTGRES_NAME')
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
