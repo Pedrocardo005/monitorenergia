51066897a27fab325b12fcebf6dc8a29058b8c60
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-23 19:39:57 -0300

    Add estrutura para exibir uma pagina no app gerenciadorenergia

--- src/gerenciadorenergia/models.py
+++ src/gerenciadorenergia/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class InfoConsumo(models.Model):
+    nome_dispositivo = models.TextField(max_length=255)
+    consumo = models.FloatField()
+    corrente = models.FloatField()
