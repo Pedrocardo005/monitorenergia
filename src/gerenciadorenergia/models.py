c48af79d9dde00a73705cc287240be7a61b32b5f
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-23 22:00:03 -0300

    Altera model para obter data que o objeto foi criado
     - Altera nome dos containers

--- src/gerenciadorenergia/models.py
+++ src/gerenciadorenergia/models.py
@@ -6,3 +6,4 @@ class InfoConsumo(models.Model):
     nome_dispositivo = models.TextField(max_length=255)
     consumo = models.FloatField()
     corrente = models.FloatField()
+    date_time = models.DateTimeField(auto_now_add=True)
