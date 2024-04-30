03748325b061451797b7140d30038f0ebf1878bb
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-04-29 23:32:16 -0300

    Atualizacao continua do grafico com algumas informacoes

--- src/gerenciadorenergia/static/js/index.js
+++ src/gerenciadorenergia/static/js/index.js
@@ -1,15 +1,19 @@
 //import {Chart} from 'chart.js/auto';
 
 const ctx = document.getElementById('myChart');
-const mockInformations = []
+const title = document.getElementById('title');
+const select = document.getElementById('select');
+const mockInformations = [];
+
+let setIntervalReturn = 0;
       
-new Chart(ctx, {
+let chart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Gráfico com consumos',
+            data: [],
             borderWidth: 1
         }]
     },
@@ -22,3 +26,49 @@ new Chart(ctx, {
         }
     },
 });
+
+select.addEventListener('change', function (event) {
+    let format_used = event.target.value;
+    
+    setIntervalReturn && clearInterval(setIntervalReturn);
+
+    getData(format_used);
+
+    setIntervalReturn = setInterval(() => {
+        getData(format_used);
+    }, 10000);    
+});
+
+function getData(format_used) {
+    fetch(`formated/${format_used}/`)
+        .then((response) => response.json())
+        .then((response) => {
+            const labels = response.items.map((element) => element.tempo);
+            const data = response.items.map((element) => element.consumo);
+            
+            title.innerHTML = response.current_device;
+            
+            chart.destroy();
+
+            chart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Gráfico com consumos',
+                        data: data,
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    responsive: false,
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                },
+            });
+        });
+}
+
